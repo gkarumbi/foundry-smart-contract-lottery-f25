cfdc2db3d58d4de4d09b4c14b32380582c5ab29c
gkarumbi <gkarumbi@gmail.com>
2025-01-07 13:24:19 +0300

    Add skeleton code.

---
+++ src/Raffle.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title A sample Raffle contract
+ * @author George Karumbi
+ * @notice This contract is from creatinf a simple raffle
+ * @dev Implement Chainlink VRF2.5
+ */
+
+contract Raffle{
+
+    function enterRaffle() public {}
+
+    function pickWinner() public {}
+
+}
+
