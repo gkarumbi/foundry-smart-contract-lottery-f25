fc137459760db924fe7463ea2def901e5e8de38b
gkarumbi <gkarumbi@gmail.com>
2025-01-07 13:29:40 +0300

    Add a getter function to return our entrance fee

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,10 +9,25 @@ pragma solidity ^0.8.19;
  */
 
 contract Raffle{
+    uint256 private immutable i_entranceFee;
 
-    function enterRaffle() public {}
+    constructor(uint256 entranceFee){
+        i_entranceFee = entranceFee;
+
+    }
+
+    function enterRaffle() public payable {
+        //Since we are going to be paying an entrace fee, we make our function payable
+
+    }
 
     function pickWinner() public {}
+
+    /** Getter Functions **/
+
+    function getEntranceFee() external view returns(uint256){
+        return i_entranceFee;
+    }
 
 }
 
